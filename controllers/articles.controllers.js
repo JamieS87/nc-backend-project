2af21e41796a0740b4121b559456457270afcedd
Jamie Sitch <sitchjamie@gmail.com>
2023-11-20 16:26:34 +0000

    Adds tests and implementation of GET /api/articles endpoint. Updates endpoints.json with the new endpoint and moves all tests into one file

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,4 +1,7 @@
-const { selectArticleById } = require("../models/articles.models");
+const {
+  selectArticleById,
+  selectArticles,
+} = require("../models/articles.models");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
@@ -9,4 +12,11 @@ exports.getArticleById = (req, res, next) => {
     })
     .catch(next);
 };
-//test
+
+exports.getArticles = (req, res, next) => {
+  selectArticles()
+    .then((articles) => {
+      res.status(200).send({ articles });
+    })
+    .catch(next);
+};
--- models/articles.models.js
+++ models/articles.models.js
@@ -16,3 +16,17 @@ exports.selectArticleById = (article_id) => {
   });
 };
 //test
+exports.selectArticles = () => {
+  const queryString = `
+  SELECT articles.author, articles.title, articles.article_id, articles.topic, 
+         articles.created_at, articles.votes, articles.article_img_url,
+         COUNT(comments.comment_id)::INTEGER as comment_count
+  FROM articles
+  LEFT JOIN comments USING (article_id)
+  GROUP BY articles.article_id
+  ORDER BY created_at DESC
+  `;
+  return db.query(queryString).then(({ rows }) => {
+    return rows;
+  });
+};
