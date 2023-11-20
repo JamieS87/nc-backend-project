471b929dbf0ff1f7f8c4c444c9aff590ed230d15
Jamie Sitch <sitchjamie@gmail.com>
2023-11-20 14:39:35 +0000

    adds GET /api/articles/:article_id endpoint with tests, error handling for postgres errors, modifies handleServerError, and adds /api/articles/:article_id endpoint to endpoints.json

--- __tests__/integration/articles.test.js
+++ __tests__/integration/articles.test.js
@@ -20,20 +20,19 @@ describe("/api/articles/:article_id", () => {
       .then(({ body }) => {
         const { article } = body;
         expect(article).toMatchObject({
-          title: "Living in the shadow of a great man",
-          topic: "mitch",
-          author: "butter_bridge",
-          body: "I find this existence challenging",
-          created_at: 1594329060000,
-          votes: 100,
-          article_img_url:
-            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          title: expect.any(String),
+          topic: expect.any(String),
+          author: expect.any(String),
+          body: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
         });
       });
   });
 
   test("GET: 404 responds with appropriate message when attempting to GET an article that doesn't exist", () => {
-    const { testArticleData } = testData;
+    const { articleData: testArticleData } = testData;
     return request(app)
       .get(`/api/articles/${testArticleData.length + 1}`)
       .expect(404)
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { handle404, handleServerError } = require("./errors");
+const {
+  handle404,
+  handleServerError,
+  handlePostgresError,
+} = require("./errors");
 const { getTopics } = require("./controllers/topics.controllers");
 const { getArticleById } = require("./controllers/articles.controllers");
 
@@ -10,6 +14,7 @@ app.get("/api/articles/:article_id", getArticleById);
 
 app.all("*", handle404);
 
+app.use(handlePostgresError);
 app.use(handleServerError);
 
 exports.app = app;
