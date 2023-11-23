bb8689cf29aec3532bec914c243230542d460793
Jamie Sitch <sitchjamie@gmail.com>
2023-11-23 11:07:09 +0000

    refactors code to use express routers

---
+++ routes/api-router.js
@@ -0,0 +1,15 @@
+const { getAPI } = require("../controllers/api.controllers");
+const articlesRouter = require("./articles-router");
+const commentsRouter = require("./comments-router");
+const topicsRouter = require("./topics-router");
+const usersRouter = require("./users-router");
+
+const apiRouter = require("express").Router();
+
+apiRouter.get("/", getAPI);
+apiRouter.use("/topics", topicsRouter);
+apiRouter.use("/articles", articlesRouter);
+apiRouter.use("/comments", commentsRouter);
+apiRouter.use("/users", usersRouter);
+
+module.exports = apiRouter;
