b09771ebba6836d42234046d0e306ecee2bfdc3c
Jamie Sitch <sitchjamie@gmail.com>
2023-11-23 12:23:49 +0000

    adds patch /api/comments/:comment_id endpoint with tests

--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,7 +1,10 @@
-const { deleteComment } = require("../controllers/comments.controllers");
+const {
+  deleteComment,
+  patchComment,
+} = require("../controllers/comments.controllers");
 
 const commentsRouter = require("express").Router();
 
-commentsRouter.route("/:comment_id").delete(deleteComment);
+commentsRouter.route("/:comment_id").patch(patchComment).delete(deleteComment);
 
 module.exports = commentsRouter;
