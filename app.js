9b33da8fbd4d32621614c097733d123ef53b3904
Jamie Sitch <sitchjamie@gmail.com>
2023-12-09 18:37:19 +0000

    allows cross origin resource sharing

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const {
   handleServerError,
   handlePostgresError,
 } = require("./errors");
+const cors = require("cors");
 
 const apiRouter = require("./routes/api-router");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
